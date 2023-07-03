3b4c05ebe348637f1c6c07982fb425b12ff647c8
Jorge Lozano <jlozanoa6@ucentral.edu.co>
2023-07-03 14:58:20 -0500

    add scripts of installation

---
+++ docker.sh
@@ -0,0 +1,18 @@
+sudo apt-get update
+sudo apt-get install ca-certificates curl gnupg
+
+#Add Docker’s official GPG key
+sudo install -m 0755 -d /etc/apt/keyrings
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+sudo chmod a+r /etc/apt/keyrings/docker.gpg
+
+#Setup repository
+echo \
+  "deb [arch="$(dpkg --print-architecture)" signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
+  "$(. /etc/os-release && echo "$VERSION_CODENAME")" stable" | \
+  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+
+#Install docker
+sudo apt-get update -y
+
+sudo apt-get install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
---
+++ install-terraform.sh
@@ -0,0 +1,16 @@
+#Dependencies
+sudo apt-get update && sudo apt-get install -y gnupg software-properties-common
+
+#Install GPG key
+wget -O- https://apt.releases.hashicorp.com/gpg | \
+gpg --dearmor | \
+sudo tee /usr/share/keyrings/hashicorp-archive-keyring.gpg
+
+#Add repository
+echo "deb [signed-by=/usr/share/keyrings/hashicorp-archive-keyring.gpg] \
+https://apt.releases.hashicorp.com $(lsb_release -cs) main" | \
+sudo tee /etc/apt/sources.list.d/hashicorp.list
+
+sudo apt update -y
+
+sudo apt-get install -y terraform 
